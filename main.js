21dea906def48ce0b4cfb3f0459a0f0369e592d1
Colin Jaffe <balloonasaurus@gmail.com>
2020-04-14 15:24:05 -0400

    Initial commit.

---
+++ main.js
@@ -0,0 +1,124 @@
+const Node = (value) => {
+  return {
+    value,
+    next: null,
+  }
+}
+
+
+const Lil = () => {
+  return {
+    head: null,
+
+    addToStart: function(value) {
+      const newNode = Node(value);
+
+      if (this.head === null) {
+        this.head = newNode;
+
+        return;
+      }
+
+      newNode.next = this.head;
+      this.head.previous = newNode;
+      this.head = newNode;
+
+    },
+
+    addToEnd: function(value) {
+      const newNode = Node(value);
+
+      if (this.head === null) {
+        this.head = newNode;
+        this.tail = newNode;
+
+        return;
+      }
+      
+      let current = this.head;
+
+      while (current.next !== null) {
+        current = current.next
+      }
+
+      current.next = newNode;
+    },
+
+    removeFromStart: function() {
+      const toBeRemoved = this.head.value;
+      this.head = this.head.next;
+
+      return toBeRemoved;
+    },
+
+    removeFromEnd: function() {
+      let current = this.head;
+
+      while (current.next.next !== null) {
+        current = current.next
+      }
+
+      const toBeRemoved = current.next.value;
+      current.next = null;
+
+      return toBeRemoved;
+    },
+
+    getAt: function(i) {
+      if (this.head === null) {
+        return null;
+      }
+
+      let count = 0;
+      let current = this.head;
+
+      while (count < i) {
+        current = current.next;
+        count++;
+      }
+
+      return current.value;
+    },
+
+    removeAt: function(i) {
+      if (this.head === null) {
+        return null;
+      }
+
+      let count = 0;
+      let current = this.head;
+
+      while (count < i - 1) {
+        current = current.next;
+        count++;
+      }
+
+      const toBeRemoved = current.next;
+      const oneAfter = current.next.next;
+      const removedValue = toBeRemoved.value;
+      
+      current.next = oneAfter;
+
+      return removedValue;
+    },
+  }
+}
+
+
+
+
+
+
+if (typeof Node === 'undefined') {
+  Node = undefined;
+}
+
+if (typeof Lil === 'undefined') {
+  Lil = undefined;
+}
+
+
+module.exports = {
+  Lil,
+  Node,
+}
